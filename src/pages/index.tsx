cd4c847e8c419bd44149f1360ab9eb48d7f1afd9
isaac-holt <isaac_holt@icloud.com>
2020-09-21 14:25:15 +0100

    Fixed error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,3 @@
-import { Button, Paper, Typography } from "@material-ui/core";
-import { GetStaticProps } from "next";
-import Head from "next/head";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 import DashBoard from "../components/DashBoard";
 import LandingPage from "../components/LandingPage";
 import useIsLoggedIn from "../hooks/useIsLoggedIn";
