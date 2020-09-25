32c5ac96435524e5943c69f7dcc5e1ba8de558f1
isaac-holt <isaac_holt@icloud.com>
2020-09-25 07:57:13 +0100

    Please work!

--- src/server/getDB.ts
+++ src/server/getDB.ts
@@ -1,14 +1,23 @@
 import { MongoClient } from "mongodb";
-const client = new MongoClient("mongodb+srv://Isaac:paphIs-juqsib-kogvo8@testcluster.i2ddc.mongodb.net/data?retryWrites=true&w=majority", {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-});
+import url from "url";
+let cachedDb = null;
 export default async (db: string) => {
     try {
-        await client.connect();
-        return client.db(db);
+        if (cachedDb) {
+            console.log("cache");
+            
+            return cachedDb;
+        }
+        /*const client = new MongoClient("mongodb+srv://Isaac:paphIs-juqsib-kogvo8@testcluster.i2ddc.mongodb.net/data?retryWrites=true&w=majority", {
+            useUnifiedTopology: true,
+            //useNewUrlParser: true,
+        });*/
+        const client = await MongoClient.connect("mongodb+srv://Isaac:paphIs-juqsib-kogvo8@testcluster.i2ddc.mongodb.net/data?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+        const db = client.db(url.parse("mongodb+srv://Isaac:paphIs-juqsib-kogvo8@testcluster.i2ddc.mongodb.net/data?retryWrites=true&w=majority").pathname.substr(1));
+        cachedDb = db;
+        return db;
     } catch (err) {
-        await client.close();
+        //await client.close();
         throw err;
     }
 }
