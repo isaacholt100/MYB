f21543746ca3e03763b9e74f1159705bd4e4e0a3
isaac-holt <isaac_holt@icloud.com>
2020-09-27 10:10:16 +0100

    JWT fix error

--- src/server/tryCatch.ts
+++ src/server/tryCatch.ts
@@ -8,7 +8,7 @@ export default async (res: NextApiResponse, fn: (() => Promise<void>) | (() => v
         console.error("error:");
         console.error(err);
         res.status(Number.isNaN(+err.message) ? 500 : +err.message).json({
-            err: err.toString() + " " + process.env.ACCESS_TOKEN + " " + process.env.REFRESH_TOKEN,
+            err: err.toString() + " " + process.env.ACCESS_TOKEN + " " + process.env.REFRESH_TOKEN + " " + process.env.MONGODB_URI,
         });
     }
 }
