d23696c3f47c44ca230fffc8f1ed1083d60fef6e
isaac-holt <isaac_holt@icloud.com>
2020-10-27 16:42:55 +0000

    Small changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -262,7 +262,7 @@ function ThemeWrapper({ children }: { children: ReactChild }) {
     return (
         <>
             <Head>
-                <link rel="stylesheet" href={fontFamily} />
+                <link rel="preload" href={fontFamily} />
             </Head>
             <MuiTheme theme={muiTheme}>
                 <div className={"flex flex_col full_screen"}>
@@ -369,6 +369,9 @@ export default ({ Component, pageProps }: AppProps) => {
                     }
                 }}
             >
+                <Head>
+                    <title>Squool</title>
+                </Head>
                     <Theme>
                         <Pickers utils={DateUtils}>
                             <Snackbar
@@ -395,20 +398,6 @@ export default ({ Component, pageProps }: AppProps) => {
                                 }}
                                 maxSnack={4}
                             >
-                                <Head>
-                                    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-                                    <meta charSet="utf-8" />
-                                    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-                                    <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
-                                    <meta name="description" content="<Description Here>" />
-                                    <meta name="keywords" content="<Keywords here>" />
-                                    <link rel="manifest" href="/manifest.json" />
-                                    <link href="/favicons/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
-                                    <link href="/favicons/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
-                                    <link rel="apple-touch-icon" href="/favicons/apple-icon.png"></link>
-                                    <meta name="theme-color" content="#317EFB" />
-                                    <title>Squool</title>
-                                </Head>
                                 <ThemeWrapper>
                                     <Component {...pageProps} />
                                 </ThemeWrapper>
