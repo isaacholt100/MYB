3b5c4852810a46c557bdae0e8b1342e77abccd12
isaac-holt <isaac_holt@icloud.com>
2020-10-31 14:22:29 +0000

    Update loading page styles

--- src/components/LoadPreview.tsx
+++ src/components/LoadPreview.tsx
@@ -6,12 +6,12 @@ export default function LoadPreview({ status, getData }) {
         <div className={styles.root}>
             {status === "loading" ? (
                 <>
-                    <Typography variant="h5" gutterBottom color="inherit">Loading your data...</Typography>
+                    <Typography variant="h5" gutterBottom color="inherit" className={styles.text}>Loading your data...</Typography>
                     <CircularProgress color="inherit" disableShrink />
                 </>
             ) : (
                 <>
-                    <Typography variant="h5" gutterBottom className={styles.errorText}>
+                    <Typography variant="h5" gutterBottom className={styles.error_text}>
                         There was an error loading your data
                     </Typography>
                     <Button color="default" onClick={getData}>Retry</Button>
