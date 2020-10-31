6547aa0d1352f33df0b199f1537c5f92eabb690a
isaac-holt <isaac_holt@icloud.com>
2020-10-31 13:50:21 +0000

    Testing

--- src/components/LoadPreview.tsx
+++ src/components/LoadPreview.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Typography, Button, CircularProgress } from "@material-ui/core";
 import styles from "../css/loadPreview.module.css";
-export default function LoadPreview ({ status, getData, ...props }) {
+export default function LoadPreview({ status, getData }) {
     return (
-        <div className={styles.root} {...props}>
+        <div className={styles.root}>
             {status === "loading" ? (
                 <>
                     <Typography variant="h5" gutterBottom color="inherit">Loading your data...</Typography>
