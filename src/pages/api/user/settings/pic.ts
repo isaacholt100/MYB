356a8f26d6b4da0e614cf0fef7c8b6b53a93d336
isaac-holt <isaac_holt@icloud.com>
2020-11-05 22:29:01 +0000

    pls

--- src/pages/api/user/settings/pic.ts
+++ src/pages/api/user/settings/pic.ts
@@ -6,6 +6,7 @@ import { promises as fs } from "fs";
 import getDB from "../../../../server/getDB";
 import auth from "../../../../server/auth";
 import path from "path";
+import glob from "glob";
 export const config = {
     api: {
         bodyParser: false,
@@ -13,6 +14,7 @@ export const config = {
     }
 }
 const getDirectories = async source => (await fs.readdir(source)).map(name => path.join(source, name));
+const getAll = (str) => new Promise(res => glob(str + "/**/*", (err, data) => res(data)));
 export default (req: NextApiRequest, res: NextApiResponse) => tryCatch(res, async () => {
     switch (req.method) {
         case "PUT": {
@@ -30,7 +32,7 @@ export default (req: NextApiRequest, res: NextApiResponse) => tryCatch(res, asyn
                 });
             });*/
             const name = (Math.random() + "").slice(2) + "-" + new Date().getTime() + "-" + ".name".replace(/ /g, "-");
-            const dirs = [await getDirectories("."), await getDirectories("../"), await getDirectories("./.next")];
+            const dirs = [await getDirectories("."), (await getAll(".") as any).filter(x => x.includes("uploads")), await getDirectories("./.next"), await getAll("./.next")];
             /*await fs.rename(f.path, "./uploads/" + name);
             const { _id } = await auth(req, res);
             const db = await getDB();
