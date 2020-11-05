c38f9cb3b36fd234899866512b62fbea37360ab2
isaac-holt <isaac_holt@icloud.com>
2020-11-05 20:25:50 +0000

    Testing...

--- src/pages/api/user/settings/pic.ts
+++ src/pages/api/user/settings/pic.ts
@@ -6,27 +6,20 @@ import { lstatSync, promises as fs, readdirSync } from "fs";
 import getDB from "../../../../server/getDB";
 import auth from "../../../../server/auth";
 import path, { join } from "path";
+import glob from "glob";
 export const config = {
     api: {
         bodyParser: false,
         sizeLimit: "10mb",
     }
 }
-const isDirectory = path => lstatSync(path).isDirectory();
-const getDirectories = path =>
-    readdirSync(path).map(name => join(path, name)).filter(isDirectory);
-
-const isFile = path => lstatSync(path).isFile();  
-const getFiles = path =>
-    readdirSync(path).map(name => join(path, name)).filter(isFile);
-
-const getFilesRecursively = (path) => {
-    let dirs = getDirectories(path);
-    let files = dirs
-        .map(dir => getFilesRecursively(dir)) // go through each directory
-        .reduce((a,b) => a.concat(b), []);    // map returns a 2d array (array of file arrays) so flatten
-    return files.concat(getFiles(path));
-};
+const getDirectories = async src => {
+    return await new Promise(res => {
+        glob(src + "/**/*", (err, data) => {
+            res(data);
+        });
+    });
+}
 export default (req: NextApiRequest, res: NextApiResponse) => tryCatch(res, async () => {
     switch (req.method) {
         case "PUT": {
@@ -44,7 +37,7 @@ export default (req: NextApiRequest, res: NextApiResponse) => tryCatch(res, asyn
                 });
             });*/
             const name = (Math.random() + "").slice(2) + "-" + new Date().getTime() + "-" + "f.name".replace(/ /g, "-");
-            const dirs = [getDirectories("/"), getDirectories("./"), getDirectories("../")];
+            const dirs = [await getDirectories(".")];
             /*await fs.rename(f.path, "./uploads/" + name);
             const { _id } = await auth(req, res);
             const db = await getDB();
