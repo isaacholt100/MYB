6597dc6fe4f27aa4f02d576c2a246cb1609c8798
isaac-holt <isaac_holt@icloud.com>
2020-11-05 22:36:15 +0000

    pls

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const withPWA = require("next-pwa");
 const WorkerPlugin = require("worker-plugin");
 module.exports = withPWA({
+    serverRuntimeConfig: {
+        PROJECT_ROOT: __dirname,
+    },
     pwa: {
         disable: process.env.NODE_ENV !== "production",
         dest: "public",
