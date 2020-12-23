33b55f68dfc30687cd71cad8408a64922381de5c
isaac-holt <isaac_holt@icloud.com>
2020-12-23 19:57:46 +0000

    Remove console logs

--- src/pages/api/user/settings/quote.ts
+++ src/pages/api/user/settings/quote.ts
@@ -7,13 +7,9 @@ import tryCatch from "../../../../server/tryCatch";
 export default (req: NextApiRequest, res: NextApiResponse) => tryCatch(res, async () => {
     switch (req.method) {
         case "PUT": {
-            console.log(req.body.quote);
-            
             if (!req.body.quote || req.body.quote.length > 150) {
                 throw new Error("Name too long");
             }
-            console.log("ok");
-            
             const { _id } = await auth(req, res);
             const db = await getDB();
             const users = db.collection("users");
